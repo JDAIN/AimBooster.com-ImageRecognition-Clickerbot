22edb0a1e439eb8c055aecc64f6d8af8d43dd83e
JDAIN <poker98@web.de>
2018-11-25 22:05:46 +0100

    startbutton autoclick and speed improvments

--- aimBoosterBot.py
+++ aimBoosterBot.py
@@ -10,32 +10,35 @@ def click_start(delay=5):
         center_challenge = pyautogui.center(
             pyautogui.locateOnScreen('challenge.PNG'))
         pyautogui.click(center_challenge)
-        return
+        return 'start'
     except:
         print('challenge not found')
         pass
     try:
         center_again = pyautogui.center(pyautogui.locateOnScreen('again.PNG'))
         pyautogui.click(center_again)
-        return
+        return 'again'
     except:
         print('again not found')
         pass
     try:
         center_start = pyautogui.center(pyautogui.locateOnScreen('start.PNG'))
         pyautogui.click(center_start)
-        return
+        return 'start'
     except:
         print('start not found')
         pass
 # color rgb 255,219,195 point center light color
-# TODO click startbutton
+
 # TODO identify playarea
 
 
 if __name__ == '__main__':
-    click_start(5)
+    a = None
+    while a is None:
+        a = click_start()
 
+    print('none over')
     offset_x = 658
     offset_y = 340
     # left, top, width, and height
@@ -53,10 +56,21 @@ if __name__ == '__main__':
         b_query = 195
         a = np.where((data[:, :, r] == r_query) & (
             data[:, :, g] == g_query) & (data[:, :, b] == b_query))
+        # if a[0].size > 0:
+        #     # TODO optimize targetswitch
+        #     for i in range(0, a[0].size, 250):
+        #         x = offset_x + a[1][i]
+        #         y = offset_y + a[0][i]
+        #         pyautogui.click(x=x, y=y)
         if a[0].size > 0:
-            # TODO optimize targetswitch
-            for i in range(0, a[0].size, 250):
-                x = offset_x + a[1][i]
-                y = offset_y + a[0][i]
-                pyautogui.click(x=x, y=y)
-        # print(a)
+            # firstclick
+            x = offset_x + a[1][0]
+            y = offset_y + a[0][0]
+            pyautogui.click(x=x, y=y)
+            for i in range(1, a[0].size - 1):
+                x = a[0][i+1] - a[0][i]
+                if x > 1:
+                    xx = offset_x + a[1][i+1]
+                    yy = offset_y + a[0][i+1]
+                    pyautogui.click(x=xx, y=yy)
+                    print('gap')
